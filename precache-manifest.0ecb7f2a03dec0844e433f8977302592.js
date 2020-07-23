0cb7659894e0d2a191ac15a00d5c1e2264992240
Alberto Aurensanz <aaurensanz@hiberus.com>
2020-07-23 11:51:19 +0200

    Updates

---
+++ precache-manifest.0ecb7f2a03dec0844e433f8977302592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee9d55ad08800d42c5302f034e52392",
+    "url": "/health-monitor/index.html"
+  },
+  {
+    "revision": "dd4874f8aefefd55c54c",
+    "url": "/health-monitor/static/css/main.82fc01fd.chunk.css"
+  },
+  {
+    "revision": "84a433eb308ac140c95f",
+    "url": "/health-monitor/static/js/2.1c1ad720.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/health-monitor/static/js/2.1c1ad720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4874f8aefefd55c54c",
+    "url": "/health-monitor/static/js/main.abef077f.chunk.js"
+  },
+  {
+    "revision": "90272489c6888ef51054",
+    "url": "/health-monitor/static/js/runtime-main.32565af2.js"
+  }
+]);
